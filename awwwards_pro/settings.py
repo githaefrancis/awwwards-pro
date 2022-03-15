70e4cbe295f1011753cd27e667edbf1fe99a441b
Francis Githae <francis.githae@student.moringaschool.com>
2022-03-15 20:55:19 +0300

    add logout redirect

--- awwwards_pro/settings.py
+++ awwwards_pro/settings.py
@@ -169,6 +169,7 @@ LOGIN_REDIRECT_URL='home'
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
+LOGOUT_REDIRECT_URL='home'
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS=[os.path.join(BASE_DIR,"static")]
