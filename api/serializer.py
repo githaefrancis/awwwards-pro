264d1565f2502fb490cf40b20db70f16cc9cca96
Francis Githae <francis.githae@student.moringaschool.com>
2022-03-16 04:41:07 +0300

    display user projects via the api

--- api/serializer.py
+++ api/serializer.py
@@ -1,10 +1,23 @@
+from dataclasses import fields
 from rest_framework import serializers
-from projects.models import UserProfile
+from projects.models import UserProfile,Project
 from django.contrib.auth.models import User
 
 
+class ProjectSerializer(serializers.ModelSerializer):
+  class Meta:
+    model=Project
+    fields=['title','description']
+
+class UserSerializer(serializers.ModelSerializer):
+  project=ProjectSerializer(many=True)
+  class Meta:
+    model=User
+    fields=['username','email','project']
+
 
 class UserProfileSerializer(serializers.ModelSerializer):
+  user=UserSerializer(many=False)
   class Meta:
     model=UserProfile
-    fields=('user','name','bio','profile_photo')
+    fields=('user','name','bio','profile_photo',)
